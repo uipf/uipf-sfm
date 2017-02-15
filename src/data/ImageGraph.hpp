70327117454f35bc582171cd48caf0016b6a8905
Carsten Brandt <mail@cebe.cc>
2017-02-15 22:41:01 +0100

    visualize images graph and matches

--- src/data/ImageGraph.hpp
+++ src/data/ImageGraph.hpp
@@ -1,6 +1,8 @@
 #ifndef UIPFSFM_DATA_SFMIMAGEGRAPH_HPP
 #define UIPFSFM_DATA_SFMIMAGEGRAPH_HPP
 
+#include <sstream>
+
 #include <uipf/data.hpp>
 #include <uipf/data/list.hpp>
 #include "Image.hpp"
@@ -41,6 +43,47 @@ namespace uipfsfm {
 			 */
 			std::map<std::string, std::string> tags;
 			std::map<std::string, uipf::Data::ptr> data;
+
+
+			/**
+			 * @return a list of visualization options.
+			 */
+			virtual std::vector<std::string> visualizations() {
+				std::vector<std::string> v;
+				// TODO do not offer this option if graphviz is not installed
+				v.push_back("graph");
+				return v;
+			};
+
+			virtual void visualize(std::string option, uipf::VisualizationContext& context) {
+				if (option.compare("graph") == 0) {
+					// TODO choose temp path by uipf or directly pipe to commansd
+					std::ofstream s("/tmp/uipfsfm-graph.dot");
+					s << "strict graph {\n";
+//					s << "  rankdir=\"LR\";\n";
+
+					for(ImagePair::ptr pair: getContent()) {
+
+						Image::ptr imageA = pair->getContent().first;
+						Image::ptr imageB = pair->getContent().second;
+						std::string fileA = boost::filesystem::path(imageA->getContent()).filename().string();
+						std::string fileB = boost::filesystem::path(imageB->getContent()).filename().string();
+
+						s << "\"" << fileA << "\" -- \"" << fileB << "\";";
+					}
+					s << "}\n";
+					s.close();
+//					uipf_exec_stdout("dot -Tpng /tmp/uipfsfm-graph.dot -o /tmp/uipfsfm-graph.png");
+					uipf_exec_stdout("neato /tmp/uipfsfm-graph.dot | dot -Tpng -o /tmp/uipfsfm-graph.png");
+					// TODO proper error handling, uipf::ErrorException
+					uipf::data::OpenCVMat::ptr m = uipf::data::load_image_color("/tmp/uipfsfm-graph.png");
+//					boost::filesystem::remove(boost::filesystem::path("/tmp/uipfsfm-graph.dot"));
+					boost::filesystem::remove(boost::filesystem::path("/tmp/uipfsfm-graph.png"));
+					context.displayImage(m->getContent());
+					return;
+
+				}
+			}
 
 		UIPF_DATA_TYPE_END
 	}
--- src/data/ImagePair.hpp
+++ src/data/ImagePair.hpp
@@ -5,8 +5,6 @@
 #include <uipf/data/list.hpp>
 #include "Image.hpp"
 
-// TODO check if external 'C' is needed here
-
 namespace uipfsfm {
 	namespace data {
 
@@ -22,6 +20,60 @@ namespace uipfsfm {
 			std::map<std::string, std::string> tags;
 			std::map<std::string, uipf::Data::ptr> data;
 
+
+			/**
+			 * @return a list of visualization options.
+			 */
+			virtual std::vector<std::string> visualizations() {
+				std::vector<std::string> v;
+				v.push_back("image pair");
+				if (hasKeyPointMatches) {
+					v.push_back("point matches");
+				}
+				return v;
+			};
+
+			virtual void visualize(std::string option, uipf::VisualizationContext& context) {
+				if (option.compare("image pair") == 0 || option.compare("point matches") == 0) {
+
+					Image::ptr imageA = getContent().first;
+					Image::ptr imageB = getContent().second;
+					cv::Mat mA = cv::imread(imageA->getContent(), CV_LOAD_IMAGE_COLOR);
+					if (!mA.data) {
+						throw uipf::ErrorException(std::string("failed to load file: ") + imageA->getContent());
+					}
+					cv::Mat mB = cv::imread(imageB->getContent(), CV_LOAD_IMAGE_COLOR);
+					if (!mB.data) {
+						throw uipf::ErrorException(std::string("failed to load file: ") + imageB->getContent());
+					}
+
+					// image must be the same type otherwise we can not be sure to display them
+					// TODO maybe add some conversion here? unlikely to happen though
+					assert(mA.type() == mB.type());
+
+					cv::Mat viz(std::max(mA.rows, mB.rows), mA.cols + mB.cols, mA.type());
+					mA.copyTo(viz(cv::Rect(0, 0, mA.cols, mA.rows)));
+					mB.copyTo(viz(cv::Rect(mA.cols, 0, mB.cols, mB.rows)));
+
+					if (option.compare("point matches") == 0 && hasKeyPointMatches) {
+						// draw matches
+						uipf_cforeach(match, keyPointMatches) {
+
+							cv::KeyPoint kA = imageA->keypoints->getContent()[match->first];
+							cv::KeyPoint kB = imageB->keypoints->getContent()[match->second];
+							cv::Point pB = kB.pt;
+							pB.x += mA.cols;
+							cv::line(viz, kA.pt, pB, cv::Scalar(0, 255, 0));
+
+						}
+					}
+
+					// TODO optional cv::drawKeypoints(img, points->getContent(), img, cv::Scalar(0,0,255), cv::DrawMatchesFlags::DRAW_OVER_OUTIMG + cv::DrawMatchesFlags::DRAW_RICH_KEYPOINTS);
+
+					context.displayImage(viz);
+				}
+
+			};
 		UIPF_DATA_TYPE_END
 	}
 }
